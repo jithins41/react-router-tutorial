750c39311870afbb1842f2e9607b94d8fdd23ae0
Jithin S <jithins41@gmail.com>
2021-07-03 17:50:01 +0530

    Basic routing finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Navbar from "./Components/Navbar";
+import{BrowserRouter as Router,Route,Link} from 'react-router-dom'
+import Index from './Components/Index'
+import About from './Components/about'
 
 function App() {
   return (
     <div>
       <h1>Hi iam from App.js</h1>
+      <Navbar></Navbar>
+
+      <Router>
+        <Link className='btn btn-primary mr-5' to='/about'> About </Link>
+        <Link className='btn btn-primary mr-3' to='/index'>Index</Link>
+        <Route component={About} path='/about'/>
+        <Route component={Index} path='/index'/>
+      </Router>
+
     </div>
   );
 }
